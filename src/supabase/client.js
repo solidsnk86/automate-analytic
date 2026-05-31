c169ba4266c6745c65d8ee9754c7554efc8046ec
solidsnk86 <calcagni.gabriel86@gmail.com>
2026-05-30 23:54:17 -0300

    remove loadEnvFile

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,5 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-process.loadEnvFile(".env");
-
 export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANNON_KEY);
